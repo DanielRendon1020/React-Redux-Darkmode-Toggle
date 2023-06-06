9771efe957269297848ed1a426b8e3196e281b7d
DanielRendon1020 <d.danny1020@gmail.com>
2023-06-05 20:41:45 -0700

    darkmode toggle

--- src/App.js
+++ src/App.js
@@ -2,12 +2,25 @@ import './App.css';
 import Nav from './components/Nav'
 import ContentWrapper from './components/ContentWrapper'
 import Footer from './components/Footer'
+import { useSelector, useDispatch } from 'react-redux';
+import { setDarkMode, setLightMode } from './features/modeSlice';
 
 function App() {
+  const mode = useSelector((state) => state);
+  const dispatch = useDispatch();
+
+  const onModeChange = (e) => {
+    const actionCreator = mode.darkMode ? setLightMode : setDarkMode;
+    dispatch(actionCreator());
+  }
+
   
   return (
     <div style={{ backgroundColor: 'white', color: 'black' }} className="App">
       <Nav />
+      <button
+      type='button'
+      onClick={onModeChange}>Toggle Dark Mode</button>
       <ContentWrapper />
       <Footer />
     </div>
